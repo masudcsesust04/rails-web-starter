5c0d32611271c36b06e5cdce3506208da1a18269
Masud Rana <masudcsesust04@gmail.com>
2017-01-25 23:54:22 +0600

    Initial commit: prepared rails bootstrap application using rails 5, devise gem, haml template engine, rspec testing framework and twitter bootstrap css framework.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+
+    respond_to do |format|
+      format.html
+    end
+  end
+end
---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsBootstrap
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Disable inherited resource
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+      g.view_specs = false
+      g.test_framework :rspec, fixture: false
+      g.factory_girl dir: 'spec/factories', suffix: 'factory'
+    end
+  end
+end
