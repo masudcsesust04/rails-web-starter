e4b9e28003693c979944847b0442c2e81a10ca34
Masud Rana <masudcsesust04@gmail.com>
2017-03-11 13:51:26 +0600

    - Updated rails version to 5.0.2 - Fix admin user seed issue

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.1'
+gem 'rails', '~> 5.0.2'
 
 # Use Rubocop as code style checking tool
 gem 'rubocop', '~> 0.47.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts '=== creating user ==='
 User.create!(
     email: 'admin@example.com',
-    password: '111111',
+    password: '11111111',
     password_confirmation: '11111111') if User.find_by_email('admin@example.com').nil?
 puts '=== end ==='
