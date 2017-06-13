ec68d333cdd2c54bc4d4905fe192409073d2e8b4
indrajit <eendroroy@gmail.com>
2017-06-14 04:33:40 +0600

    added requires and rails version requirement

--- rails-boot-starter-template.rb
+++ rails-boot-starter-template.rb
@@ -1,5 +1,11 @@
+require 'fileutils'
+require 'shellwords'
+
+RAILS_REQUIREMENT = '~> 5.0.0'
+
 def apply_template!
   @template_source = add_template_repository_to_source_path
+  assert_minimum_rails_version
   cleanup_files
   apply 'app/template.rb'
   apply 'config/template.rb'
@@ -27,6 +33,16 @@ def add_template_repository_to_source_path
     source_paths.unshift(File.dirname(__FILE__))
     File.dirname(__FILE__)
   end
+end
+
+def assert_minimum_rails_version
+  requirement = Gem::Requirement.new(RAILS_REQUIREMENT)
+  rails_version = Gem::Version.new(Rails::VERSION::STRING)
+  return if requirement.satisfied_by?(rails_version)
+
+  prompt = "This template requires Rails #{RAILS_REQUIREMENT}. "\
+           "You are using #{rails_version}. Continue anyway?"
+  exit 1 if no?(prompt)
 end
 
 def rm_dir(path)
