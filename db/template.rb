23b4e7d89eb029d83723bf1708cd312591403a27
indrajit <eendroroy@gmail.com>
2017-06-14 03:31:04 +0600

    update paths in sub templates

--- db/template.rb
+++ db/template.rb
@@ -1,3 +1,3 @@
-copy_file 'migrate/20170124115250_devise_create_users.rb'
-copy_file 'schema.rb'
-copy_file 'seeds.rb'
+copy_file 'db/migrate/20170124115250_devise_create_users.rb'
+copy_file 'db/schema.rb'
+copy_file 'db/seeds.rb'
