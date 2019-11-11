6b6f67d7307c533f5e1c6a1befbbcdf3d67d2016
Md. Masud Rana <masudcsesust04@gmail.com>
2019-11-11 07:13:53 +0600

    - changed layout banner text to 'Rails web starter - bundle install command added ruby version to the Gemfile.loc because of adding ruby verion in Gemfile - clean up seed data codebase

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-puts '=== creating user ==='
-User.create!(
-    email: 'admin@example.com',
-    password: '11111111',
-    password_confirmation: '11111111') if User.find_by_email('admin@example.com').nil?
-puts '=== end ==='
+puts 'Importing seed data....'
+unless User.exists?(email: 'admin@example.com')
+  User.create!(email: 'admin@example.com', password: '11111111',password_confirmation: '11111111')
+end    
+
+# add seeds above this line
+puts 'Done'
+
