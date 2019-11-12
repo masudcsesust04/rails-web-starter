b500698e9701523737da19f833b4fcc9ca3fdbbc
Md. Masud Rana <masudcsesust04@gmail.com>
2019-11-12 18:15:34 +0600

    - Updated application name to RailsBootStart to RailsWebStarter - Seed data cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts 'Importing seed data....'
 unless User.exists?(email: 'admin@example.com')
-  User.create!(email: 'admin@example.com', password: '11111111',password_confirmation: '11111111')
+  User.create!(email: 'admin@example.com', password: '11111111', password_confirmation: '11111111')
 end    
 
 # add seeds above this line
