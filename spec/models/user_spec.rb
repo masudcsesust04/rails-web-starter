2ad2be73a24ea4688e9ff8859590ac359ebe2d2d
Md. Masud Rana <masudcsesust04@gmail.com>
2019-11-29 00:48:01 +0600

    - Added and configured respec test coverage for devise user routes and user model. - Added db configuraiton for test environment database.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe 'validations' do
     it 'should be invalid if email is not present' do
       expect(build(:user, email: nil)).to_not be_valid
@@ -18,6 +16,5 @@ RSpec.describe User, type: :model do
     it 'should be invalid if password is not present' do
       expect(build(:user, password: nil)).to_not be_valid
     end
-
   end
 end
