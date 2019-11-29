a762472db2173f126ff1ceb6d6d94e8bc792858a
Md. Masud Rana <masudcsesust04@gmail.com>
2019-11-29 16:48:27 +0600

    Updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 puts 'Importing seed data....'
 unless User.exists?(email: 'admin@example.com')
-  User.create!(email: 'admin@example.com', password: '11111111', password_confirmation: '11111111')
+  User.create!(email: 'admin@example.com', password: '123123', password_confirmation: '123123')
 end    
 
 # add seeds above this line
 puts 'Done'
-
