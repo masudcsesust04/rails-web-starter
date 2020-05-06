1367dd48548cfb60e7b44d615c66be369503e66d
Md. Masud Rana <masudcsesust04@gmail.com>
2020-05-07 01:50:19 +0600

    - Added resource routes for user controller - Added users controller to manage application users - Disabled devise user registration routes - Added and implemented CRUD for users - Added linkable top nav menu - Disabled devices shared links for user registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, skip: 'registrations', controllers: { users: 'users', registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/users/sign_in', to: 'devise/sessions#new'
 
   mount Sidekiq::Web => '/sidekiq'
-  
+
+  resources :users
+
   root to: 'home#index'
 end
