1d82b1dfc851f791aebf1911aa96ff0a5c1f9c51
Md. Masud Rana <masudcsesust04@gmail.com>
2020-05-13 02:46:30 +0600

    - Added role model attribute validations - Added roles seed data

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,4 @@
 class Role < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 puts 'Importing seed data....'
 unless User.exists?(email: 'admin@example.com')
   User.create!(email: 'admin@example.com', password: '123123', password_confirmation: '123123')
-end    
+end
+
+if Role.count.zero?
+  Role.create!(name: 'Administrator', description: 'Admin of the application can be able to perform any operation', published: true)
+  Role.create!(name: 'Moderator', description: 'Moderator of the application can be able to perform assigned permissions', published: true)
+  Role.create!(name: 'Editor', description: 'Editor of the application can be able to perform edit of the assigned permissions', published: true)
+  Role.create!(name: 'Viewer', description: 'Viewer of the application can be able to perform view only anything.', published: true)
+end
 
 # add seeds above this line
 puts 'Done'
