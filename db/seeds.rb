a7abb9b4a02275c4298d4ecfc5b7a49c8b1c1a35
Md. Masud Rana <masudcsesust04@gmail.com>
2020-05-15 02:19:50 +0600

    - Updated seed data to assign user role for first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts 'Importing seed data....'
 unless User.exists?(email: 'admin@example.com')
-  User.create!(email: 'admin@example.com', password: '123123', password_confirmation: '123123')
+  User.create!(name: 'admin', email: 'admin@example.com', password: 'Admin@123', password_confirmation: 'Admin@123')
 end
 
 if Role.count.zero?
@@ -8,6 +8,14 @@ if Role.count.zero?
   Role.create!(name: 'Moderator', description: 'Moderator of the application can be able to perform assigned permissions', published: true)
   Role.create!(name: 'Editor', description: 'Editor of the application can be able to perform edit of the assigned permissions', published: true)
   Role.create!(name: 'Viewer', description: 'Viewer of the application can be able to perform view only anything.', published: true)
+end
+
+# assing admin role to the user
+user = User.first
+unless user.nil?
+  role = Role.first
+  user.roles << role
+  user.save
 end
 
 # add seeds above this line
