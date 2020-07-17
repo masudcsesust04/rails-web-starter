4262ce2e09e65d2250e91cd2c729772ae0b99552
Md. Masud Rana <masudcsesust04@gmail.com>
2020-07-18 01:41:03 +0600

    - Removed extra line before class definition

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class TodosController < ApplicationController
 
   before_action :authenticate_user!
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class Todo < ApplicationRecord
 
   belongs_to :creator, class_name: 'User'
