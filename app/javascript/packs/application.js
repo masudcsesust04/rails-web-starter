6655f598f2c65a041164c8d87ca924af78697d7a
Md. Masud Rana <masudcsesust04@gmail.com>
2021-02-10 14:32:10 +0600

    - Upgraded twitter boostrap 5 and fixed jquery twitter bootstrap js not working issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,22 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import 'bootstrap'
-import './src/application.scss'
+import * as bootstrap from 'bootstrap'
+import "../stylesheets/application"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
